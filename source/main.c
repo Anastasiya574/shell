fac017dfac433c5f8f7c2334aed90d02464ea2c2
Anastasiya574 <pak.nastya2206@gmail.com>
2020-10-16 09:02:46 +0600

    version 2

--- source/main.c
+++ source/main.c
@@ -8,7 +8,31 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
-void do_conv_for_two(char **list, char **A, char **B);
+char *get_word(char *end);
+char **get_list();
+void clear(char **list);
+int is_end(char **list);
+int is_delimiter(char **list, int **head);
+int redirect(char **cmd, int head);
+void run(char **list, int *head);
+
+int main() {
+    char **cmd = get_list();
+    int *head;
+    head = malloc(1 * sizeof(int));
+    head[0] = 0;
+    while(is_end(cmd) == 0) {
+        run(cmd, &head);
+        clear(cmd);
+        free(head);
+        head = malloc(1 * sizeof(int));
+        head[0] = 0;
+        cmd = get_list();
+    }
+    clear(cmd);
+    free(head);
+    return 0;
+}
 
 char *get_word(char *end) {
 	if ((*end) == '\n') {
@@ -18,7 +42,7 @@ char *get_word(char *end) {
 	char *word = (char*)malloc(sizeof(char));
 	int i = 0;
 	symbol = getchar();
-	while ((symbol != ' ') && (symbol != '\t') && (symbol != '\n')) {
+	while ((symbol != ' ') && (symbol != '\n')) {
 		word = realloc(word, i + 2);
 		if (!word) {
 			printf("Word allocation error");
@@ -59,10 +83,9 @@ void clear(char **list) {
         free(list[i]);
         i++;
     }
-		free(list[i]);
+    free(list[i]);
     free(list);
 }
-
 
 int is_end(char **list) {
 
@@ -73,116 +96,102 @@ int is_end(char **list) {
     }
 }
 
-
-
-int redirect(char **cmd){
-    int fd;
+int is_delimiter(char **list, int **head) {
     int i = 0;
-    int check = 0;
-    pid_t pid;
-    if ((pid = fork()) == 0) {
-        while (cmd[i] != NULL) {
-            if (cmd[i][0] == '>') {
-                //printf("finded\n" );
-                if (cmd[i + 1]!= NULL) {
-                    fd = open(cmd[i + 1], O_WRONLY | O_CREAT | O_TRUNC,
-                                          S_IRUSR | S_IWUSR);
-                    check = 1;
-                }
-                break;
-            }
-            if (cmd[i][0] == '<') {
-                if (cmd[i + 1]!= NULL) {
-                    fd = open(cmd[i + 1], O_RDONLY);
-                    check = 2;
-                }
-                break;
-            }
-            i++;
-        }
-        if (fd == -1) {
-            perror("file is not open");
-            return 1;
-        }
-        if (check == 1) {
-            dup2(fd, 1);
-            cmd[i] = 0;
-            free(cmd[i + 1]);
-        }
-        if (check == 2) {
-            dup2(fd, 0);
-            cmd[i] = 0;
-            free(cmd[i + 1]);
-        }
-        execvp(cmd[0], cmd);
-    }
-    wait(0);
-    return 0;
-}
-
-int if_conv_for_two(char **list) {
-    int i = 0;
-    int check = 0;
-    //int *iconv = NULL;
-    while (list[i] != NULL) {
+    int counter = 0;
+    while (list[i]) {
         if (strcmp(list[i], "|") == 0) {
-            check = i;
+            free(list[i]);
+            list[i] = NULL;
+            counter++;
+            *head = realloc(*head, (counter + 1) * sizeof(int));
+            (*head)[counter] = i + 1;
         }
         i++;
     }
-    if (check != 0) {
-        int i, j;
-        char **A = (char**)malloc((check + 1) * sizeof(char*));
-        for (i = 0; i < check; i++) {
-            A[i] = list[i];
-        }
-        A[i] = NULL;
-        i = check + 1;
-        int k = i;
-        while (list[i] != NULL) i++;
-        char **B = (char**)malloc((i - check) * sizeof(char*));
-        for (j = 0; k < i; j++, k++) {
-            B[j] = list[k];
-        }
-        B[j] = NULL;
-        do_conv_for_two(list, A, B);
-    }
-    return check;
+    return counter;
 }
 
-void do_conv_for_two(char **list, char **A, char **B) {
-    int fd[2];
-    pipe(fd);
-    if (fork() == 0) {
-        dup2(fd[1], 1);
-        close(fd[0]);
-        close(fd[1]);
-        execvp(A[0], A);
+int redirect(char **cmd, int head) {
+    int fd_in = 0, fd_out = 0;
+    int i = head;
+    while (cmd[i] != NULL) {
+        if (cmd[i][0] == '>') {
+            fd_out = open(cmd[i + 1],
+                O_WRONLY | O_CREAT | O_TRUNC ,
+                S_IRUSR | S_IWUSR);
+            dup2(fd_out, 1);
+            free(cmd[i]);
+            cmd[i] = NULL;
+            free(cmd[i + 1]);
+            cmd[i + 1] = NULL;
+            i++;
+            close(fd_out);
+        }
+        if (cmd[i][0] == '<') {
+            fd_in =  open(cmd[i + 1], O_RDONLY);
+            dup2(fd_in, 0);
+            free(cmd[i]);
+            cmd[i] = NULL;
+            free(cmd[i + 1]);
+            cmd[i + 1] = NULL;
+            i++;
+            close(fd_in);
+        }
+        i++;
     }
-    if (fork() == 0) {
-        dup2(fd[0], 0);
-        close(fd[0]);
-        close(fd[1]);
-        execvp(B[0], B);
-    }
-    close(fd[0]);
-    close(fd[1]);
-    wait(NULL);
-    wait(NULL);
-    free(A);
-    free(B);
-    return;
-}
-
-int main(int argc, char** argv) {
-    char **cmd = NULL;
-		cmd = get_list();
-		while(is_end(cmd) == 0) {
-			redirect(cmd);
-            if_conv_for_two(cmd);
-			clear(cmd);
-			cmd = get_list();
-		}
-		clear(cmd);
     return 0;
+}
+
+
+void run(char **list, int *head) {
+        int pipe_in_phone = 0;
+        if (list[0] != NULL) {
+            pipe_in_phone = is_delimiter(list, &head);
+            int (*fd)[2] = malloc((pipe_in_phone + 1) * sizeof(int[2]));
+            for (int i = 0; i <= pipe_in_phone; i++) {
+                pipe(fd[i]);
+            }
+            if (fork() == 0) {
+                if (pipe_in_phone != 0) {
+                     dup2(fd[0][1], 1);
+                }
+                close(fd[0][1]);
+                close(fd[0][0]);
+
+                redirect(list, head[0]);
+                if (execvp(list[head[0]], list+head[0]) < 0) {
+                     perror("exec failed");
+                }
+            for (int i = 1; i <= pipe_in_phone; i++) {
+                if (fork() == 0) {
+                    dup2(fd[i - 1][0], 0);
+                    close(fd[i - 1][0]);
+                    close(fd[i - 1][1]);
+
+                    if (i != pipe_in_phone) {
+                        dup2(fd[i][1], 1);
+                    }
+                    close(fd[i][1]);
+                    close(fd[i][0]);
+
+                    redirect(list, head[i]);
+                if (execvp(list[head[i]], list+head[i]) < 0) {
+                     perror("exec failed");
+                } 
+                } else {
+                    close(fd[i - 1][1]);
+                    close(fd[i - 1][0]);
+                }
+            }
+            close(fd[pipe_in_phone][1]);
+            close(fd[pipe_in_phone][0]);
+
+            for (int i = 0; i <= pipe_in_phone; i++) {
+                wait(NULL);
+            }
+            free(fd);
+        }
+    return;
+    }
 }
